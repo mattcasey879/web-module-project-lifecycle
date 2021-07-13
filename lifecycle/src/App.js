a02beda3f545539d7e74192b2e340ae85e354526
Matt <matthew-casey@lambdastudents.com>
2021-07-12 19:48:47 -0500

    added didupdate method

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -8,18 +8,36 @@ const Api = 'https://api.github.com/users'
 
 class App extends React.Component {
   state = {
-    user: {},
+    username: 'mattcasey879',
+    userData: {},
     followers: [],
     display: false
   }
   componentDidMount() {
-    axios.get(`${Api}/mattcasey879`)
-    .then(res => this.setState({user: res.data}))
+    axios.get(`${Api}/${this.state.username}`)
+    .then(res => this.setState({...this.state, userData: res.data}))
     .catch(err => alert(err))
-    axios.get(`${Api}/mattcasey879/followers`)
+    axios.get(`${Api}/${this.state.username}/followers`)
     .then(res => this.setState({...this.state, followers: res.data}))
-    .catch(err => alert(err + 'Could not get follwers!'))
+    .catch(err => alert(err + ' Could not get follwers!'))
+    
   }
+
+  componentDidUpdate(prevProps, prevState) {
+   if(prevState.username !== this.state.username){
+    axios.get(`${Api}/${this.state.username}`)
+    .then(res => this.setState({...this.state, userData: res.data}))
+    .catch(err => alert(err))
+    axios.get(`${Api}/${this.state.username}/followers`)
+    .then(res => this.setState({...this.state, followers: res.data}))
+    .catch(err => alert(err + ' Could not get follwers!'))
+   }
+  }
+
+
+onSubmitNewUser = (newUsername) => {
+  this.setState({...this.state, username: newUsername})
+}
 
   toggleFollowers = () => {
     this.setState({...this.state, display: !this.state.display})
@@ -28,12 +46,13 @@ class App extends React.Component {
   render(){
     return (
         <div className='App'>
-         {Object.keys(this.state.user).length === 0 ? <h3 id='loading'>Loading...</h3> : 
-         <User data={this.state.user}
-          toggleFollowers={this.toggleFollowers}/>}
+         {Object.keys(this.state.userData).length === 0 ? <h3 id='loading'>Loading...</h3> : 
+         <User username={this.state.username}
+          toggleFollowers={this.toggleFollowers}
+          onSubmitNewUser={this.onSubmitNewUser}/>}
           <div>
           
-           {this.state.followers.length === 0 ? <div></div> : 
+           {this.state.followers.length === 0 ? <div><h4> Loading...</h4></div> : 
            this.state.display && <UserFollowers 
            data={this.state.followers}
            />}
--- lifecycle/src/Components/User.js
+++ lifecycle/src/Components/User.js
@@ -2,17 +2,34 @@ import React from 'react';
 
 
 class User extends React.Component {
+
+    state = {
+        user: ''
+    }
+
+    onChangeUser = (e) => {
+        this.setState({user: e.target.value})
+    }
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        this.props.onSubmitNewUser(this.state.user)
+    }
     render() {
-        const { data , toggleFollowers } = this.props
+        const { username , toggleFollowers } = this.props
         return (
             <div className='user-container'>
                 <div className='App-header'>
                     <h2>Current User:</h2>
-                    <h4>{data.name}</h4>
+                    <h4>{username}</h4>
                     
                 </div>
+                <form>
+                    <input placeholder='change username' value={this.user} onChange={this.onChangeUser}></input>
+                     <button onClick={this.onSubmitForm}>Submit</button>
+                </form>
                 <div className='intro'>
-                    <p>Username: {data.login}</p>
+                    <p>Username: {username}</p>
                     <button onClick={toggleFollowers} className='btn-primary'>Click to Show followers</button>
                 </div>
             </div>
